bf0fc005ae6173a5652b741fc366288dfacf898c
DESKTOP-SQOO6RH\carlo <carlos.calderonn@pucp.pe>
2021-04-03 18:15:08 -0500

    creacion del modulo cali -cali

---
+++ cali/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from cali import views
+
+urlpatterns = [
+    # path( 'URL A LA CUAL APUNTO', BACKEND, name= Nombre de la variable)
+    path('home/', views.caliHome, name='caliHome' )
+]
+
+
+
+
+
+
--- demosoftware3/urls.py
+++ demosoftware3/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('leon/', include('leon.urls')),
     path('raul/', include('raul.urls')),
-    path('lozano/', include('lozano.urls'))
+    path('lozano/', include('lozano.urls')),
+path('cali/', include('cali.urls'))
 ]
