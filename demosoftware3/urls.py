094e8e88f09330af467dde770bc4d6e7fc9dba35
Raul140298 <raul.jeri@pucp.edu.pe>
2021-04-03 17:59:23 -0500

    Creacion de urls de Raúl - raul - demosoftware3

--- demosoftware3/urls.py
+++ demosoftware3/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('leon/', include('leon.urls'))
+    path('leon/', include('leon.urls')),
+    path('raul/', include('raul.urls'))
 ]
