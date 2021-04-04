840b5cda3a0d6083a33cc53bb940c2fe4a88168f
diego_loza <dlozano099@gmail.com>
2021-04-04 18:52:29 -0500

    Creacion base de crud - lozano

--- lozano/urls.py
+++ lozano/urls.py
@@ -6,6 +6,9 @@ from lozano import views
 
 urlpatterns = [
     # path( 'URL A LA CUAL APUNTO', BACKEND, name= Nombre de la variable)
-    path('home/', views.lozanoHome, name='lozanoHome' ),
-    path('404/', views.lozano404, name='lozano404')
+    path('home/', views.lozanoHome, name='lozanoHome'),
+    path('idiomas/', views.lozanoIdiomas, name='lozanoIdiomas'),
+    path('404/', views.lozano404, name='lozano404'),
+    path('idioma/<pk>', views.lozanoIdioma, name="lozanoIdioma"),
+    path('pais/<pk>', views.lozanoPais, name="lozanoPais")
 ]
