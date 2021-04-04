f3505580ed38a44ba792428b3d98b0d1af03dff1
JHONATAN\user <jhonatan.cueva@pucp.edu.pe>
2021-04-04 15:20:50 -0500

    Creacion de models.py - Jhonatan

--- Jhonatan/views.py
+++ Jhonatan/views.py
@@ -1,14 +1,27 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
-
+from leon.models import Language
 
 
 def PolarHome(request):
+
+    langList = Language.objects.filter(habloElIdioma=False)
+    print("=======================================================================")
+    print(langList)
+    print("=======================================================================")
+
+    pais = "peru"
+    response = requests.get('https://restcountries.eu/rest/v2/name/'+ pais)
+    print(response.json())
+    print("======================")
+    print(response.json()[0]["capital"])
+    capital = response.json()[0]["capital"]
+
     context = {
-        'titulo': 'Hola mundo, Soy Jhonatan',
+        'titulo': 'Hola mundo',
+        'listaIdiomas': langList,
+        'capital' : capital
     }
     return render(request, 'jhonatan/home.html', context)
-
-
-
