0ba106c3786f9f8fbca35ffc30bc72a83146f2d4
Leo <a20130263@pucp.pe>
2021-04-05 00:12:42 -0500

    Creacion url leandro - settings.py

--- leandro/views.py
+++ leandro/views.py
@@ -1,11 +1,50 @@
-
+import requests
 from django.shortcuts import render
 
 # Create your views here.
+from leandro.models import Prueba
+
 
 def leandroHome(request):
+
+    print("-----------------------------------------------------------------------")
+    print(request.POST)
+    print("-----------------------------------------------------------------------")
+    if request.POST:
+
+        Prueba.objects.create(name=request.POST["username"], habloElIdioma=False, longitud= len(request.POST["username"]))
+
+        context = {
+            'titulo': "Has guardado en BBDD :" + request.POST["username"],
+            'capital': "POST"
+        }
+        return render(request, 'leandro/home.html', context)
+
+    langList = Prueba.objects.filter()
+    print("=======================================================================")
+    print(langList)
+    print("=======================================================================")
+
+    pais = "peru"
+    response = requests.get('https://restcountries.eu/rest/v2/name/'+ pais)
+    print(response.json())
+    print("======================")
+    print(response.json()[0]["capital"])
+    capital = response.json()[0]["capital"]
+
     context = {
-        'body': 'Hola mundo  zzzz',
+        'titulo': 'HE ENTADO POR URL',
+        'listaIdiomas': langList,
+        'capital' : capital,
+        'body':'soy un string',
     }
     return render(request, 'leandro/home.html', context)
 
+
+def idiomaDetalle(request, pk):
+    idioma = Prueba.objects.get(pk=pk)
+
+    context = {
+        "idiomaDetalle": idioma
+    }
+    return render(request, 'leandro/idiomas/idioma_detalle.html', context)
