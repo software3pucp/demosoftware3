155b71bf388ffb66ac8f5e5fc36f09e06c0646aa
Aaronou <70769296+Aaronou@users.noreply.github.com>
2021-04-11 15:04:33 -0500

    Creación de las vistas de Insertar y listar - aaron

--- aaron/views.py
+++ aaron/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render
+import requests
+from django.shortcuts import render
+
+# Create your views here.
+from aaron.models import Waffles
+
 
 # Create your views here.
 
 
-
 def aaronHome(request):
+
+    wafflesList = Waffles.objects.filter()
+
+
+    if request.POST:
+
+        if request.POST["tipoform"] == "1":
+
+            Waffles.objects.create(name=request.POST["name"], precio=request.POST["precio"])
+
+        context = {
+            'titulo': "Has guardado en BBDD :" + request.POST["name"],
+            'listaWaffles': wafflesList,
+        }
+        return render(request, 'aaron/home.html', context)
+
+
+
     context = {
-        'titulo': 'Hola mundo',
+        'titulo': 'HE ENTRADO POR URL',
+        'listaWaffles': wafflesList,
     }
+
+
     return render(request, 'aaron/home.html', context)
-from django.shortcuts import render
 
 # Create your views here.
