48bf52e4deee6859a0eda3ec6237da8e27ea8966
Aaronou <70769296+Aaronou@users.noreply.github.com>
2021-04-17 17:42:13 -0500

    Creación de funciones editar eliminar - aaron

--- aaron/urls.py
+++ aaron/urls.py
@@ -1,5 +1,3 @@
-
-
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,6 +5,9 @@ from aaron import views
 
 urlpatterns = [
     # path( 'URL A LA CUAL APUNTO', BACKEND, name= Nombre de la variable)
-    path('home/', views.aaronHome, name='aaronHome')
+    path('home/', views.aaronHome, name='aaronHome'),
+    path('listaWaffles/', views.listaWaffles, name='listaWaffles'),
+    path('detalle/<pk>', views.detalleWaffles, name='detalleWaffles'),
+    path('editar/<pk>', views.editarWaffles, name='editarWaffles'),
+    path('eliminar/<pk>', views.eliminarWaffles, name='eliminarWaffles')
 ]
-
--- aaron/views.py
+++ aaron/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 from django.shortcuts import render
 
@@ -10,30 +10,70 @@ from aaron.models import Waffles
 
 
 def aaronHome(request):
-
     wafflesList = Waffles.objects.filter()
-
 
     if request.POST:
 
         if request.POST["tipoform"] == "1":
-
             Waffles.objects.create(name=request.POST["name"], precio=request.POST["precio"])
 
         context = {
             'titulo': "Has guardado en BBDD :" + request.POST["name"],
             'listaWaffles': wafflesList,
         }
-        return render(request, 'aaron/home.html', context)
 
-
+        return render(request, 'aaron/listaWaffles.html', context)
 
     context = {
         'titulo': 'HE ENTRADO POR URL',
         'listaWaffles': wafflesList,
     }
 
-
     return render(request, 'aaron/home.html', context)
 
+
 # Create your views here.
+def listaWaffles(request):
+    wafflesList = Waffles.objects.filter()
+    context = {
+        'titulo': 'Lista de Waffles',
+        'listaWaffles': wafflesList,
+    }
+
+    return render(request, 'aaron/listaWaffles.html', context)
+
+
+def detalleWaffles(request, pk):
+    waffle = Waffles.objects.get(pk=pk)
+
+    print(waffle)
+
+    context = {
+        'waffle': waffle
+    }
+    return render(request, 'aaron/detalle.html', context)
+
+
+def editarWaffles(request, pk):
+    waffle = Waffles.objects.get(pk=pk)
+    if request.POST:
+        print(request.POST)
+        newname = request.POST["name"]
+        newprecio = request.POST["precio"]
+
+        waffle.name = newname
+        waffle.precio = newprecio
+        waffle.save()
+
+        return redirect(listaWaffles)
+
+    context = {
+        'waffle': waffle
+    }
+    return render(request, 'aaron/editar.html', context)
+def eliminarWaffles(request,pk):
+
+    waffle = Waffles.objects.get(pk=pk)
+    waffle.delete()
+    return redirect(listaWaffles)
+
