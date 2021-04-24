4a3a6d5419d4bd1606e5f89796ddce667fc24071
Ariz1109 <miguel.zapata@pucp.edu.pe>
2021-04-24 18:13:17 -0500

    Editar y Eliminar Crud - Ale

--- ale/urls.py
+++ ale/urls.py
@@ -6,5 +6,7 @@ from ale import views
 urlpatterns = [
     # path( 'URL A LA CUAL APUNTO', BACKEND, name= Nombre de la variable)
     path('home/', views.aleHome, name='aleHome' ),
-    path('lista/',views.usuario_list, name='aleList')
+    path('lista/',views.usuario_list, name='aleList'),
+    path('eliminar/<pk>', views.eliminarPersonajes, name='eliminarPersonajes'),
+    path('editar/<pk>', views.editarPersonajes, name='editarPersonajes')
 ]
--- ale/views.py
+++ ale/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 # Create your views here.
 from django.shortcuts import render
 from ale.models import Personaje
@@ -9,6 +9,29 @@ from ale.models import Personaje
 def usuario_list(request):
     usuario = Personaje.objects.all()
     return render(request, 'ale/base/lista.html', {'usuario':usuario})
+
+def eliminarPersonajes(request,pk):
+    usuario = Personaje.objects.get(pk=pk)
+    usuario.delete()
+    return redirect(usuario_list)
+
+def editarPersonajes(request,pk):
+    usuario = Personaje.objects.get(pk=pk)
+    if request.POST:
+        print(request.POST)
+        newname = request.POST["nombre"]
+        newedad = request.POST["edad"]
+
+        usuario.name = newname
+        usuario.edad = newedad
+        usuario.save()
+
+        return redirect(usuario_list)
+
+    context = {
+        'usuario': usuario
+    }
+    return render(request, 'ale/editar.html', context)
 
 def aleHome(request):
     print(request.POST)
