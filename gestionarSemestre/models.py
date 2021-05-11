2ab8e03db46e60d7017826138cb98746fdd1ade0
LAPTOP-BL4CHF1T\juanc <jcasasz@pucp.edu.pe>
2021-05-11 00:05:51 -0500

    Avance de añadir semestre - gestionarSemestre

--- gestionarSemestre/models.py
+++ gestionarSemestre/models.py
@@ -6,5 +6,5 @@ class Semestre(models.Model):
     nombreCodigo = models.CharField(max_length=10)
     anho = models.IntegerField()
     etapa = models.IntegerField()
-    inicio = models.DateField()
-    fin = models.DateField()
+    inicio = models.DateTimeField(null =True, blank= True)
+    fin = models.DateTimeField(null =True, blank= True)
--- gestionarSemestre/views.py
+++ gestionarSemestre/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-
-
-
-
+from datetime import datetime
+from gestionarSemestre.models import Semestre
 
 def listarSemestre(request):
-
-    # AQUI HAGO COSAS
+    semestreLista = Semestre.objects.filter()
 
     context = {
+        'ListaSemestre':semestreLista
 
     }
     return render(request,'gestionarSemestre/listarSemestre.html',context)
