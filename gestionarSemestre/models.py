84f01f3d66135c533e4fd7cceecceac03ad42e6d
DESKTOP-D97I9TR\carlo <carlos.calderonn@pucp.pe>
2021-05-11 23:42:55 -0500

    Fix de gestion Semestres -gestionarSemestres

--- gestionarSemestre/models.py
+++ gestionarSemestre/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Semestre(models.Model):
     nombreCodigo = models.CharField(max_length=10)
-    anho = models.IntegerField()
+    anho = models.IntegerField(null=True)
     etapa = models.IntegerField(null=True)
     inicio = models.CharField(max_length=20, null=True)
     fin = models.CharField(max_length=20, null=True)
