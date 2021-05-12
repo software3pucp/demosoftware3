e07da557f9880518f9f00cc362db1ca3af8be46e
LAPTOP-BL4CHF1T\juanc <jcasasz@pucp.edu.pe>
2021-05-12 17:48:14 -0500

    Validacion de form - gestionarSemestre

--- gestionarSemestre/views.py
+++ gestionarSemestre/views.py
@@ -15,7 +15,24 @@ def listarSemestre(request):
 def agregarSemestre(request):
     anho = int(request.POST["nombreCodigo"][0:4])
     etapa = int(request.POST["nombreCodigo"][5:6])
+    meses = {
+        "01": "Enero",
+        "02": "Febrero",
+        "03": "Marzo",
+        "04": "Abril",
+        "05": "Mayo",
+        "06": "Junio",
+        "07": "Julio",
+        "08": "Agosto",
+        "09": "Septiembre",
+        "10": "Octubre",
+        "11": "Noviembre",
+        "12": "Diciembre",
+    }
+
+    fechaIni = request.POST["inicio"][0:2] + " de "+meses[request.POST["inicio"][3:5]]
+    fechaFin = request.POST["fin"][0:2] + " de "+meses[request.POST["fin"][3:5]]
     semestre = Semestre.objects.create(nombreCodigo=request.POST["nombreCodigo"], anho=anho,
-                            etapa=etapa, inicio=request.POST["inicio"], fin=request.POST["fin"])
+                            etapa=etapa, inicio=fechaIni, fin=fechaFin)
     ser_instance = serializers.serialize('json', [semestre,])
     return JsonResponse({"nuevoSemestre": ser_instance}, status=200)
