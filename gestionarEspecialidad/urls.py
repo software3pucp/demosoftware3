a72510bf25eb6d802aeb0029026c79531ca76e27
Ariz1109 <miguel.zapata@pucp.edu.pe>
2021-05-12 21:55:51 -0500

    Correccion gestionarFacultad - urls

--- gestionarEspecialidad/urls.py
+++ gestionarEspecialidad/urls.py
@@ -4,8 +4,8 @@ from gestionarEspecialidad import views
 
 urlpatterns = [
     # path( url hijo, backend, nombre de la variable)
-    path('/listar', views.listarEspecialidad, name="listarEspecialidad"),
-    path('/agregar', views.agregarEspecialidad, name="agregarEspecialidad"),
+    path('listar/', views.listarEspecialidad, name="listarEspecialidad"),
+    path('agregar/', views.agregarEspecialidad, name="agregarEspecialidad"),
     # path('editar', views.editarFacultad, name="editarFacultad"),
     # path('eliminar', views.eliminarFacultad, name="eliminarFacultad"),
 ]
