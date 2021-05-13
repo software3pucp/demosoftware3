9a59b3333a9b207f9dd98940426f17c42bc4acc1
Ariz1109 <miguel.zapata@pucp.edu.pe>
2021-05-13 12:54:44 -0500

    Listar Especialidad por Facultad - gestionarFacultad

--- gestionarFacultad/urls.py
+++ gestionarFacultad/urls.py
@@ -5,7 +5,8 @@ from gestionarFacultad import views
 urlpatterns = [
     # path( url hijo, backend, nombre de la variable)
     path('listar/', views.listarFacultad, name="listarFacultad"),
+    path('listar/<pk>', views.listarFacultadxEsp, name="listarFacultadxEsp"),
     # path('editar', views.editarFacultad, name="editarFacultad"),
     # path('eliminar', views.eliminarFacultad, name="eliminarFacultad"),
-    path('crear/',views.crearFacultad, name="crearFacultad")
+    path('crear/', views.crearFacultad, name="crearFacultad")
 ]
