377e964e6d7aa201528ac505d88d774a5789560c
Raul140298 <raul.jeri@pucp.edu.pe>
2021-05-12 23:00:13 -0500

    Creacion de gestionarFacultades - gestionarFacultades

--- gestionarEspecialidad/models.py
+++ gestionarEspecialidad/models.py
@@ -1,20 +1,25 @@
 from django.db import models
+from gestionarFacultad import models as FacuModels
 import unicodedata
+
 
 def remove_accents(input_str):
     nfkd_form = unicodedata.normalize('NFKD', input_str)
     return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
+
 # Create your models here.
 def substring_after(s, delim):
     return s.rpartition(delim)[-1]
 
+
 def upload_location(instance, filename):
-    extension = substring_after(filename,'.')
+    extension = substring_after(filename, '.')
     return 'img/%s.%s' % (remove_accents(instance.nombre), extension)
+
 
 class Especialidad(models.Model):
     nombre = models.CharField(max_length=30)
     responsable = models.CharField(max_length=50)
+    facultad = models.ForeignKey(FacuModels.Facultad, on_delete=models.CASCADE, null=True)
     foto = models.ImageField(null=True, blank=True, upload_to=upload_location)
-
--- gestionarFacultad/views.py
+++ gestionarFacultad/views.py
@@ -1,24 +1,22 @@
-from django.shortcuts import render
+from gestionarFacultad.models import Facultad
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
-
-
-
-
-
-
 def listarFacultad(request):
-
-    # AQUI HAGO COSAS
-
     context = {
-
+        'ListaFacultad': Facultad.objects.all(),
     }
-    return render(request,'gestionarFacultad/listarFacultad.html',context)
+    return render(request, 'gestionarFacultad/listarFacultad.html', context)
+
 
 def crearFacultad(request):
-    # AQUI HAGO COSAS
-
+    if request.POST:
+        nombre = request.POST['name']
+        id_responsable = request.POST['responsable']
+        foto = request.FILES['photo']
+        Facultad.objects.create(nombre=nombre, responsable=id_responsable, foto=foto)
+        return redirect(listarFacultad)
     context = {
 
     }
