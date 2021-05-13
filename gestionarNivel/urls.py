2e21cbb0a66fa2306046cf9aeecbdb4d39943486
gilmer.cabrera@pucp.edu.pe <gilmer.cabrera@pucp.edu.pe>
2021-05-13 14:21:07 -0500

    Fix gestionarNivel-gestionarNivel

--- gestionarNivel/urls.py
+++ gestionarNivel/urls.py
@@ -4,7 +4,9 @@ from gestionarNivel import views
 
 urlpatterns = [
     # path( url hijo, backend, nombre de la variable)
-    path('/listar', views.listarNiv, name="listarNiv"),
-    path('/editar/<pk>', views.editarNiv, name="editarNiv"),
-    path('/crear', views.crearNiv, name="crearNiv")
+    # path('/listar', views.listarNiv, name="listarNiv"),
+    # path('/editar/<pk>', views.editarNiv, name="editarNiv"),
+    # path('/crear', views.crearNiv, name="crearNiv")
+    path('/agregar', views.agregarNivel, name='agregarNivel'),
+    path('/listar', views.listarNivel, name="listarNivel"),
 ]
--- gestionarNivel/views.py
+++ gestionarNivel/views.py
@@ -1,44 +1,61 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.core import serializers
 
 from gestionarNivel.models import Nivel
 
 
-def listarNiv(request):
-    flag =  False
-    if request.POST:
-        nivel = Nivel.objects.get(pk=request.POST['nivelPk'])
-        nivel.state=0
-        nivel.save()
-        flag = True
+# def listarNiv(request):
+#     flag =  False
+#     if request.POST:
+#         nivel = Nivel.objects.get(pk=request.POST['nivelPk'])
+#         nivel.state=0
+#         nivel.save()
+#         flag = True
+#
+#     niveles= Nivel.objects.filter(state=1)
+#     context = {
+#         'nivelesList' : niveles,
+#         'flag' : flag
+#     }
+#     return render(request, 'gestionarNivel/listarNiv.html', context)
+#
+# def crearNiv(request):
+#     registrado = False
+#     if request.POST:
+#         Nivel.objects.create(name=request.POST['name'], value=request.POST['value'])
+#         registrado = True
+#     context = {
+#         'registrado': registrado,
+#     }
+#     return render(request, 'gestionarNivel/crearNiv.html', context)
+#
+# def editarNiv(request,pk):
+#     flag = False
+#     if request.POST:
+#         nivel = Nivel.objects.get(pk=pk)
+#         nivel.name = request.POST['name']
+#         nivel.value = request.POST['value']
+#         nivel.save()
+#         flag = True
+#     nivel = Nivel.objects.get(pk=pk)
+#     context = {
+#         'nivel': nivel,
+#         'flag': flag,
+#     }
+#     return render(request, 'gestionarNivel/editarNiv.html', context)
 
-    niveles= Nivel.objects.filter(state=1)
+def listarNivel(request):
+    nivelLista = Nivel.objects.filter(state=1)
     context = {
-        'nivelesList' : niveles,
-        'flag' : flag
+       'nivelLista': nivelLista
     }
-    return render(request, 'gestionarNivel/listarNiv.html', context)
+    return render(request,'gestionarNivel/listarNivel.html', context)
 
-def crearNiv(request):
-    registrado = False
-    if request.POST:
-        Nivel.objects.create(name=request.POST['name'], value=request.POST['value'])
-        registrado = True
-    context = {
-        'registrado': registrado,
-    }
-    return render(request, 'gestionarNivel/crearNiv.html', context)
+def agregarNivel(request):
+    nombre = request.POST['name']
+    valor = request.POST['value']
 
-def editarNiv(request,pk):
-    flag = False
-    if request.POST:
-        nivel = Nivel.objects.get(pk=pk)
-        nivel.name = request.POST['name']
-        nivel.value = request.POST['value']
-        nivel.save()
-        flag = True
-    nivel = Nivel.objects.get(pk=pk)
-    context = {
-        'nivel': nivel,
-        'flag': flag,
-    }
-    return render(request, 'gestionarNivel/editarNiv.html', context)
+    nivel = Nivel.objects.create(name=nombre, value=valor)
+    ser_instance = serializers.serialize('json', [nivel, ])
+    return JsonResponse({"nuevoNivel": ser_instance}, status=200)
