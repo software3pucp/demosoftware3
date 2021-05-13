e79fec356763082bcb814e21d37caee0374be438
gilmer.cabrera@pucp.edu.pe <gilmer.cabrera@pucp.edu.pe>
2021-05-13 18:03:17 -0500

    Fix gestionarNivel - gestionarNivel

--- gestionarNivel/urls.py
+++ gestionarNivel/urls.py
@@ -5,8 +5,8 @@ from gestionarNivel import views
 urlpatterns = [
     # path( url hijo, backend, nombre de la variable)
     # path('listar/', views.listarNiv, name="listarNiv"),
-    # path('editar/<pk>', views.editarNiv, name="editarNiv"),
     # path('crear/', views.crearNiv, name="crearNiv")
+    path('editar/<pk>', views.editarNiv, name="editarNiv"),
     path('agregar/', views.agregarNivel, name='agregarNivel'),
     path('listar/', views.listarNivel, name="listarNivel"),
 ]
