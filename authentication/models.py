dff043a6beb1e8e86909e67c897447c67c78a088
Raul140298 <raul.jeri@pucp.edu.pe>
2021-05-13 21:15:15 -0500

    Implementación de combobox en Especialidades - gestionarEspecialidades

--- authentication/models.py
+++ authentication/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import unicodedata
+
+
 # Create your models here.
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
+
+
+# Create your models here.
+def substring_after(s, delim):
+    return s.rpartition(delim)[-1]
+
+
+def upload_location(instance, filename):
+    extension = substring_after(filename, '.')
+    return 'img/%s.%s' % (remove_accents(instance.first_name), extension)
+
 
 class User(AbstractUser):
-    code = models.CharField(max_length=8,default='11111111')
-    photo = models.ImageField(upload_to='usuarios/', null=True)
+    code = models.CharField(max_length=8, default='11111111')
+    photo = models.ImageField(null=True, blank=True, upload_to=upload_location)
--- authentication/views.py
+++ authentication/views.py
@@ -8,16 +8,16 @@ from authentication.models import User
 
 
 def Show(request):
-
     context = {
         'users': User.objects.all(),
     }
-    return render(request,'authentication/User_list.html',context)
+    return render(request, 'authentication/User_list.html', context)
 
 
 def Register(request):
     if request.POST:
-        User.objects.create(first_name=request.POST['card-name'],username=request.POST['card-correo'],code=request.POST['card-codigo'],email=request.POST['card-correo'],password=request.POST['card-password'],is_active=True)
+        User.objects.create(photo=request.FILES['photo'], first_name=request.POST['card-name'],
+                            username=request.POST['card-correo'], code=request.POST['card-codigo'],
+                            email=request.POST['card-correo'], password=request.POST['card-password'], is_active=True)
         return redirect(Show)
-    return render(request,'authentication/User_Add.html')
-
+    return render(request, 'authentication/User_Add.html')
