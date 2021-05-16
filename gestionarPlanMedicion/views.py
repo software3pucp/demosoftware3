428db1453e0e26e424a1568dd1efd0ce4058f3b7
Jose Murillo <j.murillo@pucp.edu.pe>
2021-05-15 23:35:14 -0500

    Avance Listar Cursos - GestionarPlanMedicion

--- gestionarPlanMedicion/views.py
+++ gestionarPlanMedicion/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from gestionarCurso.models import Curso
 
 # Create your views here.
 
 def listarCursos(request):
     facultad = "Ingenieria Informática"
     estados = ["Activo","Inactivo"]
+    cursos = Curso.objects.filter(especialidad=1)
     context = {
         'facultad' : facultad,
-        'estados' : estados
+        'estados' : estados,
+        'cursos' : cursos
     }
     return render(request,'gestionarPlanMedicion/listarCursos.html',context)
 
