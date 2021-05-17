ef4b866a3ab17c884852ce0ebb5ed29096aac520
diego_loza <dlozano099@gmail.com>
2021-05-16 19:10:31 -0500

    Avance listar - gestionarPlanMedicion

--- gestionarPlanMedicion/views.py
+++ gestionarPlanMedicion/views.py
@@ -1,8 +1,9 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from gestionarCurso.models import Curso
 
 # Create your views here.
+from django.core import serializers
+from gestionarCurso.models import Curso
 from gestionarEspecialidad.models import Especialidad
 from gestionarFacultad.models import Facultad
 
@@ -10,16 +11,24 @@ from gestionarFacultad.models import Facultad
 def listarCursos(request):
 
     if request.POST:
-        print('*******************************************************************************************************')
-        print(request.POST)
-        print('*******************************************************************************************************')
-        return JsonResponse({"response": 'Correct'}, status=200)
-
+        print('***************************************************************************************************')
+        print(request)
+        print('***************************************************************************************************')
+        if request.POST['operacion'] == 'listEspe':
+            especialidades = Especialidad.objects.filter(facultad_id=request.POST['facultad'])
+            data = serializers.serialize("json", especialidades)
+            return JsonResponse({"resp": data}, status=200)
+        elif request.POST['operacion'] == 'listCur':
+            cursos = Curso.objects.filter(especialidad_id=request.POST['especialidad'])
+            data = serializers.serialize("json", cursos)
+            return JsonResponse({"resp": data}, status=200)
+    facultades = Facultad.objects.filter()
+    especialidades = Especialidad.objects.filter()
     estados = ["Activo","Inactivo"]
-    cursos = Curso.objects.filter()
     context = {
+        'facultades' : facultades,
+        'especialidades' : especialidades,
         'estados' : estados,
-        'cursos' : cursos
     }
     return render(request,'gestionarPlanMedicion/listarCursos.html',context)
 
