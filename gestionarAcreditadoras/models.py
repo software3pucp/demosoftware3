ff0e529836c4295c3b232d1179f14d1113b090f0
diego_loza <dlozano099@gmail.com>
2021-06-02 16:29:22 -0500

    Avance crearAcreditadoras - gestioanrAcreditadoras

--- gestionarAcreditadoras/models.py
+++ gestionarAcreditadoras/models.py
@@ -2,10 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
+
+
+# Create your models here.
+def substring_after(s, delim):
+    return s.rpartition(delim)[-1]
+
+
+def upload_location(instance, filename):
+    extension = substring_after(filename, '.')
+    return 'img/%s.%s' % (remove_accents(instance.nombre), extension)
+
 class Acreditadora(models.Model):
     ESTADOS = [
         ('0','Eliminado'),
         ('1','Activo'),
     ]
-    nombre = models.CharField(max_length=100,default=None,null=True,blank=True)
+    nombre = models.CharField(max_length=100,default='',null=True,blank=True)
+    foto = models.ImageField(null=True, blank=True, upload_to=upload_location)
     estado = models.CharField(max_length=2,choices=ESTADOS,default=None,null=True,blank=True)
--- gestionarAcreditadoras/views.py
+++ gestionarAcreditadoras/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+import unicodedata
 
 # Create your views here.
+from gestionarAcreditadoras.models import Acreditadora
+
 
 def listarAcreditadoras(request):
     context = {
@@ -9,7 +12,14 @@ def listarAcreditadoras(request):
     return render(request,'gestionarAcreditadoras/listarAcreditadoras.html',context)
 
 def crearAcreditadoras(request,pk):
-    context = {
+    acreditadora = Acreditadora()
+    acreditadora.pk = pk
 
+    if request.POST:
+        if request.POST['operacion'] == 'entrada':
+            print('Insertar')
+
+    context = {
+        'acreditadora': acreditadora,
     }
     return render(request,'gestionarAcreditadoras/crearAcreditadoras.html',context)
