62463680b3a153e08fe53e9bbaffba6bf84525e7
gilmer.cabrera@pucp.edu.pe <gilmer.cabrera@pucp.edu.pe>
2021-06-02 02:17:00 -0500

    fix gestionarNivel-gestionarNivel

--- gestionarNivel/urls.py
+++ gestionarNivel/urls.py
@@ -3,10 +3,7 @@ from django.urls import path, include
 from gestionarNivel import views
 
 urlpatterns = [
-    # path( url hijo, backend, nombre de la variable)
-    # path('listar/', views.listarNiv, name="listarNiv"),
-    # path('crear/', views.crearNiv, name="crearNiv")
-    path('editar/<pk>', views.editarNiv, name="editarNiv"),
-    path('agregar/', views.agregarNivel, name='agregarNivel'),
+    path('agregar/', views.agregarNivel, name="agregarNivel"),
     path('listar/', views.listarNivel, name="listarNivel"),
+    path('editar/<pk>', views.editarNiv, name="editarNiv"),
 ]
