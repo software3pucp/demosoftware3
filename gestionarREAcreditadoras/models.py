ee05ca27bffccd631c8fa2fdbfa249ff494253e4
Leo <a20130263@pucp.pe>
2021-06-07 09:03:26 -0500

    modificacion del editarRE - gestionarREAcreditadoras

--- gestionarREAcreditadoras/models.py
+++ gestionarREAcreditadoras/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-from gestionarResultados.models import ResultadoPUCP
 from gestionarAcreditadoras.models import Acreditadora
+
 
 class ResultadoAcreditadora(models.Model):
     ESTADOS = [
@@ -13,5 +13,6 @@ class ResultadoAcreditadora(models.Model):
     codigo = models.CharField(max_length=10,default='',null=True,blank=True)
     descripcion = models.CharField(max_length=150,default='',null=True,blank=True)
     estado = models.CharField(max_length=2,choices=ESTADOS,default='2',null=True,blank=True)
-    acreditadora = models.ForeignKey(Acreditadora,on_delete=models.RESTRICT)
-    resultadoPUCP = models.ForeignKey(ResultadoPUCP,on_delete=models.RESTRICT)
+    acreditadora = models.ForeignKey(Acreditadora,on_delete=models.RESTRICT, null=True)
+
+
--- gestionarREAcreditadoras/views.py
+++ gestionarREAcreditadoras/views.py
@@ -1,8 +1,10 @@
 import requests
 from django.shortcuts import render
 from gestionarREAcreditadoras.models import Acreditadora,ResultadoAcreditadora
+from gestionarIndicadores.models import Indicador
 
 # Create your views here.
+
 
 def listarRE(request):
     resultados = ResultadoAcreditadora.objects.filter(acreditadora=1)
@@ -18,11 +20,17 @@ def editarRE(request,pk):
     resultadoAcreditadora = ResultadoAcreditadora()
     resultadoAcreditadora.pk = pk
 
+
+    indicadores=Indicador.objects.filter()
+    print(indicadores)
+
     if request.POST:
+        print(request.POST)
         if request.POST['operacion'] == 'entrada':
             resultadoAcreditadora.acreditadora_id = request.POST["acreditadora"]
 
         elif request.POST['operacion'] == 'editar':
+
             resultadoAcreditadora = ResultadoAcreditadora.objects.get(pk=pk)
             resultadoAcreditadora.codigo = request.POST['codigo']
             resultadoAcreditadora.descripcion = request.POST['descripcion']
@@ -30,10 +38,15 @@ def editarRE(request,pk):
             flag = 1
 
         elif request.POST['operacion'] == 'insertar':
-            ResultadoAcreditadora.objects.create(codigo=request.POST['codigo'],descripcion=request.POST['descripcion'],
-                                                 acreditadora_id=request.POST["acreditadora"])
-            resultadoAcreditadora = ResultadoAcreditadora.objects.latest('id')
-            pk = resultadoAcreditadora.pk
+            codigo = request.POST['codigo']
+            descripcion = request.POST['descripcion']
+            # acreditadora_id = request.POST["acreditadora"]
+            # acreditadora_id = 1
+            print("AYUDAAAAAAAAAAAA")
+            ResultadoAcreditadora.objects.create(codigo=codigo,descripcion=descripcion)
+            print("AYUDA")
+            # resultadoAcreditadora = ResultadoAcreditadora.objects.latest('id')
+            # pk = resultadoAcreditadora.pk
             flag = 2
 
     if pk == '0':
@@ -44,6 +57,7 @@ def editarRE(request,pk):
     context = {
         'insert': insert,
         'resultadoAcreditadora': resultadoAcreditadora,
+        'indicadores': indicadores,
         'flag' : flag,
     }
     return render(request,'gestionarREAcreditadoras/editarRE.html',context)
