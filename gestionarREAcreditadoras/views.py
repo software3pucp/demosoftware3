68572b99d8308de5764c0208485c6242aac10c18
LAPTOP-BL4CHF1T\juanc <jcasasz@pucp.edu.pe>
2021-06-09 15:08:06 -0500

    Fix de editar e insertar de REAcreditaciones - gestionarREAcreditaciones

--- gestionarREAcreditadoras/views.py
+++ gestionarREAcreditadoras/views.py
@@ -12,17 +12,17 @@ from gestionarIndicadores.models import Indicador
 from gestionarResultados.models import ResultadoPUCP
 
 
-def listarRE(request,pk):
+def listarRE(request, pk):
     acreditadora = Acreditadora.objects.get(pk=pk)
     resultados = ResultadoAcreditadora.objects.filter(acreditadora=pk)
     context = {
         'acreditadora': acreditadora,
-        'resultados' : resultados,
+        'resultados': resultados,
     }
-    return render(request,'gestionarREAcreditadoras/listarRE.html',context)
+    return render(request, 'gestionarREAcreditadoras/listarRE.html', context)
 
 
-def editarRE(request,pk):
+def editarRE(request, pk):
     insert = False
     flag = 0
     resultadoSeleccionado = 0
@@ -31,7 +31,7 @@ def editarRE(request,pk):
     resultadoAcreditadora = ResultadoAcreditadora()
     resultadoAcreditadora.pk = pk
     acreditadora = Acreditadora.objects.get(pk=request.POST["acreditadora"])
-    facultades = Facultad.objects.filter( estado='1')
+    facultades = Facultad.objects.filter(estado='1')
     if request.POST:
         print(request.POST)
         if request.POST['operacion'] == 'entrada':
@@ -44,15 +44,26 @@ def editarRE(request,pk):
             resultadoAcreditadora = ResultadoAcreditadora.objects.get(pk=pk)
             resultadoAcreditadora.codigo = request.POST["codigo"]
             resultadoAcreditadora.descripcion = request.POST["descripcion"]
-            resultadoAcreditadora.resultadoPUCP_id = request.POST["select"]
+            if ('select' in request.POST):
+                resultadoAcreditadora.resultadoPUCP_id = request.POST["select"]
             resultadoAcreditadora.acreditadora_id = request.POST["acreditadora"]
             resultadoAcreditadora.save()
             flag = 1
             return redirect('listarRE', acreditadora.pk)
 
         elif request.POST['operacion'] == 'insertar':
-            resultadoAcreditadora = ResultadoAcreditadora.objects.create(codigo=request.POST["codigo"],descripcion=request.POST["descripcion"],
-                                                      acreditadora_id=request.POST["acreditadora"],resultadoPUCP_id=request.POST["select"])
+            if not ('select' in request.POST):
+                resultadoAcreditadora = ResultadoAcreditadora.objects.create(codigo=request.POST["codigo"],
+                                                                             descripcion=request.POST["descripcion"],
+                                                                             acreditadora_id=request.POST[
+                                                                                 "acreditadora"])
+            else:
+                resultadoAcreditadora = ResultadoAcreditadora.objects.create(codigo=request.POST["codigo"],
+                                                                             descripcion=request.POST["descripcion"],
+                                                                             acreditadora_id=request.POST[
+                                                                                 "acreditadora"],
+                                                                             resultadoPUCP_id=request.POST["select"])
+
             pk = resultadoAcreditadora.pk
             insert = True
             flag = 2
@@ -60,14 +71,14 @@ def editarRE(request,pk):
 
     if pk != '0':
         resultadoAcreditadora = ResultadoAcreditadora.objects.get(pk=pk)
-        resultadoSeleccionado = resultadoAcreditadora.resultadoPUCP_id
-        especialidadSeleccionada = ResultadoPUCP.objects.get(pk=resultadoSeleccionado).especialidad_id
-        facultadSeleccionada = Especialidad.objects.get(pk=especialidadSeleccionada).facultad_id
+        if resultadoAcreditadora.resultadoPUCP_id is not None :
+            resultadoSeleccionado = resultadoAcreditadora.resultadoPUCP_id
+            especialidadSeleccionada = ResultadoPUCP.objects.get(pk=resultadoSeleccionado).especialidad_id
+            facultadSeleccionada = Especialidad.objects.get(pk=especialidadSeleccionada).facultad_id
 
         titulo = 'Editar'
     else:
         titulo = 'Nuevo'
-
 
     context = {
         'insert': insert,
@@ -78,14 +89,16 @@ def editarRE(request,pk):
         'facultades': facultades,
         'flag': flag,
         'acreditadora': acreditadora,
-        'titulo':titulo,
+        'titulo': titulo,
     }
-    return render(request,'gestionarREAcreditadoras/editarRE.html',context)
+    return render(request, 'gestionarREAcreditadoras/editarRE.html', context)
 
-def eliminarRE(request,pk):
-    #Candidate.objects.filter(pk=pk).delete()
+
+def eliminarRE(request, pk):
+    # Candidate.objects.filter(pk=pk).delete()
     print(request.POST)
     return
+
 
 def ajaxEditar(request):
     if request.POST:
