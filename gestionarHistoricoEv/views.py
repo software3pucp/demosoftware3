5dd5a7f937075eedc60d4b0bd491ec7cf2511a1a
DESKTOP-D97I9TR\carlo <carlos.calderonn@pucp.pe>
2021-06-09 19:46:33 -0500

    Fix de listado de nombres y fechas -gestionarHistoricoEv

--- gestionarHistoricoEv/views.py
+++ gestionarHistoricoEv/views.py
@@ -15,14 +15,25 @@ def listarHistorico(request,pk):
     listaHorarios = list(Horario.objects.filter(curso_id=pk,estado=1))
     #listaDocumentos = EvidenciasxHorario.objects.filter(estado=1)
     listaDocumentos = EvidenciasxHorario.objects.none()
+    listaNombres =[]
     for i in range(len(listaHorarios)):
         listaEDocumentos = EvidenciasxHorario.objects.filter(horario_id=listaHorarios[i].id, estado=1)
         if listaEDocumentos:
+            aux = list(listaEDocumentos)
+            for j in range(len(aux)):
+                nombArchivo = str(aux[j].archivo)[8:]
+                listaNombres.append(nombArchivo)
+           # listaEDocumentos.update(archivo=nombArchivo)
             listaDocumentos = listaDocumentos|listaEDocumentos
-
+    cantidad = len(listaNombres)
+    nuevo = zip(listaNombres,listaDocumentos)
+    r = set(nuevo)
     context = {
        # 'ListaHistorico':historicoLista,
         'cursoSeleccionado': cursoSeleccionado,
-        'listaDocumentos': listaDocumentos
+        'listaDocumentos': listaDocumentos,
+        'listaNombres':listaNombres,
+        'cantidad':cantidad,
+        'listaAux':r,
     }
     return render(request, 'gestionarHistoricoEv/listarHistorico.html', context)
