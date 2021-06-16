28c98f009ee1dd0f16b8b6552dd53ab75433785e
diego_loza <dlozano099@gmail.com>
2021-06-16 16:35:58 -0500

    Crear tabla del HistoricoRE - gestionarResultados

--- gestionarResultados/models.py
+++ gestionarResultados/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 from gestionarEspecialidad.models import Especialidad
+from gestionarSemestre.models import Semestre
 
 
 class ResultadoPUCP(models.Model):
@@ -14,3 +15,16 @@ class ResultadoPUCP(models.Model):
     descripcion = models.CharField(max_length=300, default='', null=True, blank=True)
     estado = models.CharField(max_length=2, choices=ESTADOS, default='1', null=True, blank=True)
     especialidad = models.ForeignKey(Especialidad,on_delete=models.DO_NOTHING, null=True)
+
+class PlanResultados(models.Model):
+    ESTADOS = [
+        ('0', 'Eliminado'),
+        ('1', 'Activo'),
+        ('2', 'Inactivo'),
+    ]
+    codigo = models.CharField(max_length=10, default='', null=True, blank=True)
+    descripcion = models.CharField(max_length=300, default='', null=True, blank=True)
+    especialidad = models.ForeignKey(Especialidad, on_delete=models.RESTRICT)
+    semestre = models.ForeignKey(Semestre, on_delete=models.RESTRICT)
+    estado = models.CharField(max_length=2, choices=ESTADOS, default='1', null=True, blank=True)
+    resultados = models.ManyToManyField(ResultadoPUCP)
