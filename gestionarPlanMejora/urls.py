a287644386eb5285cfaf8e9608b91bd286206fee
DESKTOP-D97I9TR\carlo <carlos.calderonn@pucp.pe>
2021-06-16 19:28:41 -0500

    Fix de paths perdidos -gestionarPlanMejora

--- gestionarPlanMejora/urls.py
+++ gestionarPlanMejora/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('crearPropuesta/<id_especialidad>', views.crearPropuesta, name="crearPropuesta"),
     path('editar/<pk>', views.editarPropuesta, name="editarPropuesta"),
     path('eliminar/', views.eliminarActividadxPropuesta, name="eliminarActividadxPropuesta"),
+    path('crearActividad/', views.crearActividad, name="crearActividad"),
+    path('editarActividad/<pk>', views.editarActividad, name="editarActividad"),
+    path('subirEvidencia/<id_actividad>', views.subirEvidencia, name="subirEvidenciaActividad"),
+    path('editarEvidencia/<pk>', views.editarEvidencia, name="editarEvidenciaActividad"),
+    path('eliminarEvidencia/', views.eliminarEvidenciaxActividad, name="eliminarEvidenciaxActividad"),
 ]
