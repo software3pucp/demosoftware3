0aecb4c4eb31cd6998a57344cb7965a320f710e7
Alejandro <miguel.zapata@pucp.edu.pe>
2021-07-06 11:37:35 -0500

    Crear Nueva Tabla - User Models

--- authentication/models.py
+++ authentication/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 import unicodedata
+from django.contrib.auth.models import Group
 from django.contrib.auth.models import User
-
+from gestionarFacultad.models import Facultad
+from gestionarEspecialidad.models import Especialidad
 # Create your models here.
 def remove_accents(input_str):
     nfkd_form = unicodedata.normalize('NFKD', input_str)
@@ -20,9 +22,25 @@ def upload_location(instance, filename):
 
 
 class User(AbstractUser):
+    ESTADOS = [
+        ('0', 'Eliminado'),
+        ('1', 'Activo'),
+        ('2', 'Inactivo'),
+    ]
+    estado = models.CharField(max_length=2, choices=ESTADOS, default='1', null=True, blank=True)
     code = models.CharField(max_length=8, default='11111111')
     photo = models.ImageField(null=True, blank=True, upload_to=upload_location)
     rol_actual = models.CharField(max_length=50, default=None, null=True, blank=True)
     n_Roles = models.CharField(max_length=2, default=None, null=True, blank=True)
     token = models.UUIDField(primary_key=False, editable=False, null=True, blank=True)
+
+class UserxGroups(models.Model):
+    user = models.ForeignKey(User,on_delete= models.DO_NOTHING)
+    group = models.ForeignKey(Group,on_delete= models.DO_NOTHING)
+    facultad = models.ForeignKey(Facultad,on_delete= models.DO_NOTHING,null=True)
+    especialidad = models.ForeignKey(Especialidad,on_delete= models.DO_NOTHING,null=True)
+
+
+
+
 
