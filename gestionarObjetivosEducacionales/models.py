43e2a8b178ecf2f1b9b1770eb541a9752eae80dd
gilmer.cabrera@pucp.edu.pe <gilmer.cabrera@pucp.edu.pe>
2021-07-07 00:29:53 -0500

    gestionarObjetivosEducacionales (registrar, visualizar, editar,eliminar) - gestionarObjetivosEducacionales.

---
+++ gestionarObjetivosEducacionales/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+from gestionarEspecialidad.models import Especialidad
+
+
+class ObjetivoEducacional(models.Model):
+    ESTADOS = [
+        ('0', 'Eliminado'),
+        ('1', 'Activo'),
+        ('2', 'Inactivo'),
+    ]
+    codigo = models.CharField(max_length=10, default='', null=True, blank=True)
+    descripcion = models.CharField(max_length=300, default='', null=True, blank=True)
+    especialidad = models.ForeignKey(Especialidad, on_delete=models.RESTRICT)
+    estado = models.CharField(max_length=2, choices=ESTADOS, default='1', null=True, blank=True)
