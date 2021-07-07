b1839f09580021018fe3aa52eca4b4f4d9de7467
gilmer.cabrera@pucp.edu.pe <gilmer.cabrera@pucp.edu.pe>
2021-07-06 20:15:46 -0500

    fix de eliminar indicador- gestionarResultados/Indicadores

--- gestionarIndicadores/urls.py
+++ gestionarIndicadores/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # path( url hijo, backend, nombre de la variable)
     path('agregar/<id_resultado>/', views.agregarIndicador, name="agregarIndicador"),
     path('editar/<pk>/', views.editarIndicador,name="editarIndicador"),
-    path('eliminar/<id_resultado>/', views.eliminarIndicadorxResultado, name="eliminarIndicadorxResultado"),
+    path('eliminar/', views.eliminarIndicadorxResultado, name="eliminarIndicadorxResultado"),
     path('agregarDescripcion/', views.agregarDescipcionNivel, name="agregarDescipcionNivel"),
 ]
