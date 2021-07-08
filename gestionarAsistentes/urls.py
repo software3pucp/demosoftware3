a393a06a99929e7691d9f04ea8359529ec0591b8
gilmer.cabrera@pucp.edu.pe <gilmer.cabrera@pucp.edu.pe>
2021-07-08 12:55:41 -0500

    fix asistentes -gestionarAsistentes

--- gestionarAsistentes/urls.py
+++ gestionarAsistentes/urls.py
@@ -4,12 +4,8 @@ from gestionarAsistentes import views
 
 urlpatterns = [
     # path( url hijo, backend, nombre de la variable)
-    # Objetivos educacionales
     path('asistentes/', views.asistentes, name="asistentes"),
     path('registrar/', views.registrarAsistente, name="registrarAsistente"),
     path('listar/', views.listarAsistentes, name="listarAsistentes"),
     path('eliminar/', views.eliminarAsistente, name="eliminarAsistente"),
-    # path('editar/', views.editarObjetivo, name="editarObjetivo"),
-    #
-
 ]
