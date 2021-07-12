eae76abd83eb40c484cb417b6c45214a8eecb205
asusanibar <asusanibar@znerg.com>
2021-07-11 22:35:10 -0500

    Fix Reporte Rubricas

--- reportes/views.py
+++ reportes/views.py
@@ -120,99 +120,102 @@ def generarReportes(request):
 
         if '_rubricas' in request.POST:
             print("rubricas")
+            semestre = Semestre.objects.get(pk=ciclo)
             #Apertura de template.xlsx
             filename = os.path.join(settings.BASE_DIR, 'reportes', 'Resources', 'template.xlsx')
             wb = load_workbook(filename)
             ws = wb.active
+            try:
+                nivelesX = []
+                nivelesY=[]
+                especialidad = Especialidad.objects.get(pk=esp)
+                niveles = Nivel.objects.filter(especialidad_id=esp,estado='1')
+                for n in range(len(niveles)):
+                    if n == 0:
+                        nivelesX.append('D')
+                        nivelesY.append('B')
+                    else:
+                        nivelesX.append(chr(ord(nivelesX[n - 1]) + 1))
+                    ws[f'{nivelesX[n]}7'] = f'{niveles[n].nombre} ({niveles[n].valor})'
+                    currentcell = ws[f'{nivelesX[n]}7']
+                    currentcell.font = Font(name='Calibri', size=11, bold="True")
+                    currentcell.alignment = Alignment(horizontal='center', vertical='center', wrapText=True)
+                    currentcell.fill = PatternFill(start_color="C0C0C0", end_color="C0C0C0", fill_type="solid")
+                    if n == 0:
+                        currentcell.border = Border(left=Side(border_style='thin'), top=Side(border_style='thin'),
+                                                    bottom=Side(border_style='thin'))
+                    elif n == len(niveles) - 1:
+                        currentcell.border = Border(right=Side(border_style='thin'), top=Side(border_style='thin'),
+                                                    bottom=Side(border_style='thin'))
+                    else:
+                        currentcell.border = Border(top=Side(border_style='thin'), bottom=Side(border_style='thin'))
 
-            nivelesX = []
-            nivelesY=[]
-            especialidad = Especialidad.objects.get(pk=esp)
-            niveles = Nivel.objects.filter(especialidad_id=esp,estado='1')
-            for n in range(len(niveles)):
-                if n == 0:
-                    nivelesX.append('D')
-                    nivelesY.append('B')
-                else:
-                    nivelesX.append(chr(ord(nivelesX[n - 1]) + 1))
-                ws[f'{nivelesX[n]}7'] = f'{niveles[n].nombre} ({niveles[n].valor})'
-                currentcell = ws[f'{nivelesX[n]}7']
-                currentcell.font = Font(name='Calibri', size=11, bold="True")
-                currentcell.alignment = Alignment(horizontal='center', vertical='center', wrapText=True)
-                currentcell.fill = PatternFill(start_color="C0C0C0", end_color="C0C0C0", fill_type="solid")
-                if n == 0:
-                    currentcell.border = Border(left=Side(border_style='thin'), top=Side(border_style='thin'),
-                                                bottom=Side(border_style='thin'))
-                elif n == len(niveles) - 1:
-                    currentcell.border = Border(right=Side(border_style='thin'), top=Side(border_style='thin'),
-                                                bottom=Side(border_style='thin'))
-                else:
-                    currentcell.border = Border(top=Side(border_style='thin'), bottom=Side(border_style='thin'))
+                ws.merge_cells(f'D6:{nivelesX[len(niveles) - 1]}6')
+                ws.merge_cells(f'B5:{nivelesX[len(niveles) - 1]}5')
 
-            ws.merge_cells(f'D6:{nivelesX[len(niveles) - 1]}6')
-            ws.merge_cells(f'B5:{nivelesX[len(niveles) - 1]}5')
+                ws['C3'] = f'RÚBRICAS DEL PROGRAMA DE {especialidad.nombre.upper()}'
 
-            ws['C3'] = f'RÚBRICAS DEL PROGRAMA DE {especialidad.nombre.upper()}'
+                semAux = Semestre.objects.get(pk=ciclo)
+                planSemestre = PlanMedicion.semestre.through.objects.filter(semestre_id=semAux.pk)
+                planCal = None
+                for p in planSemestre:
+                    estado = [1, 2]
+                    planesMedi = PlanMedicion.objects.filter(pk=p.planmedicion_id, estado__in=estado, especialidad_id=esp)
+                    if planesMedi:
+                        planCal = planesMedi[0]
+                        break
 
-            semAux = Semestre.objects.get(pk=ciclo)
-            planSemestre = PlanMedicion.semestre.through.objects.filter(semestre_id=semAux.pk)
-            planCal = None
-            for p in planSemestre:
-                estado = [1, 2]
-                planesMedi = PlanMedicion.objects.filter(pk=p.planmedicion_id, estado__in=estado, especialidad_id=esp)
-                if planesMedi:
-                    planCal = planesMedi[0]
-                    break
+                plResultado = PlanResultados.objects.filter(pk=planCal.planResultados.pk)
+                if plResultado:
+                    for plR in plResultado:
+                        resTodos = ResultadoPUCP.objects.filter(planResultado_id=plR.pk, estado=1)
+                        for i in range(len(resTodos)):
+                            res = resTodos[i]
+                            codResul = res.codigo
+                            ws.title = codResul
+                            if i < len(resTodos) - 1:
+                                wb.copy_worksheet(ws)
+                                nombreAux = codResul + " Copy"
+                                ws = wb[nombreAux]
 
-            plResultado = PlanResultados.objects.filter(pk=planCal.planResultados.pk)
-            if plResultado:
-                for plR in plResultado:
-                    resTodos = ResultadoPUCP.objects.filter(planResultado_id=plR.pk, estado=1)
-                    for i in range(len(resTodos)):
-                        res = resTodos[i]
-                        codResul = res.codigo
-                        ws.title = codResul
-                        if i < len(resTodos) - 1:
-                            wb.copy_worksheet(ws)
-                            nombreAux = codResul + " Copy"
-                            ws = wb[nombreAux]
+                            wsAux = wb[codResul]
+                            wsAux['B5'] = f'{res.codigo} ({res.descripcion})'
+                            wsAux['B5'].alignment = Alignment(horizontal='center', vertical='center', wrapText=True)
+                            filaInicio = 8
+                            indImpr = Indicador.objects.filter(resultado_id=resTodos[i].pk, estado=1)
+                            if indImpr:
+                                for numInd in range(len(indImpr)):
+                                    wsAux[f'C{filaInicio}'] = f'{indImpr[numInd].codigo} {indImpr[numInd].descripcion}'
+                                    celdaEstilo = wsAux[f'C{filaInicio}']
+                                    celdaEstilo.alignment = Alignment(horizontal='left', vertical='center', wrapText=True)
+                                    celdaEstilo.border = Border(left=Side(border_style='thin'), top=Side(border_style='thin'),
+                                                                bottom=Side(border_style='thin'))
+                                    celdaEstilo.font = Font(name='Calibri', size=12, bold="True")
+                                    for n in range(len(niveles)):
+                                        rubrica = Rubrica.objects.filter(indicador_id=indImpr[numInd].pk,
+                                                                         nivel_id=niveles[n].pk)
+                                        if rubrica:
+                                            wsAux[f'{nivelesX[n]}{filaInicio}'] = rubrica[0].descripcion
+                                        rubricaEstilo = wsAux[f'{nivelesX[n]}{filaInicio}']
+                                        rubricaEstilo.alignment = Alignment(horizontal='left', vertical='center', wrapText=True)
+                                        rubricaEstilo.font = Font(name='Calibri', size=11, bold=False)
+                                        if n == len(niveles) - 1:
+                                            rubricaEstilo.border = Border(left=None,
+                                                                          right=Side(border_style='thin'),
+                                                                          top=Side(border_style='thin'),
+                                                                          bottom=Side(border_style='thin'))
+                                        else:
+                                            rubricaEstilo.border = Border(left=None,
+                                                                          right=None,
+                                                                          top=Side(border_style='thin'),
+                                                                          bottom=Side(border_style='thin'))
+                                    filaInicio += 1
+                            if filaInicio > 8:
+                                wsAux.merge_cells(f'B8:B{filaInicio - 1}')
+            except:
+                print("Semestre no tiene evaluación")
 
-                        wsAux = wb[codResul]
-                        wsAux['B5'] = f'{res.codigo} ({res.descripcion})'
-                        wsAux['B5'].alignment = Alignment(horizontal='center', vertical='center', wrapText=True)
-                        filaInicio = 8
-                        indImpr = Indicador.objects.filter(resultado_id=resTodos[i].pk, estado=1)
-                        if indImpr:
-                            for numInd in range(len(indImpr)):
-                                wsAux[f'C{filaInicio}'] = f'{indImpr[numInd].codigo} {indImpr[numInd].descripcion}'
-                                celdaEstilo = wsAux[f'C{filaInicio}']
-                                celdaEstilo.alignment = Alignment(horizontal='left', vertical='center', wrapText=True)
-                                celdaEstilo.border = Border(left=Side(border_style='thin'), top=Side(border_style='thin'),
-                                                            bottom=Side(border_style='thin'))
-                                celdaEstilo.font = Font(name='Calibri', size=12, bold="True")
-                                for n in range(len(niveles)):
-                                    rubrica = Rubrica.objects.filter(indicador_id=indImpr[numInd].pk,
-                                                                     nivel_id=niveles[n].pk)
-                                    if rubrica:
-                                        wsAux[f'{nivelesX[n]}{filaInicio}'] = rubrica[0].descripcion
-                                    rubricaEstilo = wsAux[f'{nivelesX[n]}{filaInicio}']
-                                    rubricaEstilo.alignment = Alignment(horizontal='left', vertical='center', wrapText=True)
-                                    rubricaEstilo.font = Font(name='Calibri', size=11, bold=False)
-                                    if n == len(niveles) - 1:
-                                        rubricaEstilo.border = Border(left=None,
-                                                                      right=Side(border_style='thin'),
-                                                                      top=Side(border_style='thin'),
-                                                                      bottom=Side(border_style='thin'))
-                                    else:
-                                        rubricaEstilo.border = Border(left=None,
-                                                                      right=None,
-                                                                      top=Side(border_style='thin'),
-                                                                      bottom=Side(border_style='thin'))
-                                filaInicio += 1
-                        if filaInicio > 8:
-                            wsAux.merge_cells(f'B8:B{filaInicio - 1}')
-
-            nom_arch = f'Rubrica {especialidad.nombre}.xlsx'
+            nom_arch = f'Rubrica {especialidad.nombre}-{semestre.nombreCodigo}.xlsx'
             # Definir el tipo de respuesta que se va a dar
             response = HttpResponse(content_type="application/ms-excel")
             contenido = "attachment; filename = {0}".format(nom_arch)
